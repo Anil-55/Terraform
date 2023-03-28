320aa57146ba8ebd12b10c784f5e2b837fdd6d9b
Anil-55 <85402050+Anil-55@users.noreply.github.com>
2023-03-28 15:29:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+# Provider
+ provider "aws" {
+   access_key = "AKIAVQZALNXJ6RVNSBWX"
+   secret_key = "zYv2km9v30k68zSeTiWHOqIO18g5V0kGB74uFZuv"
+   region = "us-east-1"
+ }
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
